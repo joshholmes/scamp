5d2346a0c6f32fe90f8fb6e2eb82c1dab642f803
unknown <joholmes@jhvm10.northamerica.corp.microsoft.com>
2015-07-16 16:26:49 -0700

    initial check in

---
+++ ScampWebFront/ScampWebFront/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScampWebFront.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
